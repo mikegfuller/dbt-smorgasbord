c4b0bcc8c0d99a31928e09f64c43d0eda0f2887c
Mike Fuller <mike.fuller@fishtownanalytics.com>
2021-11-02 22:32:26 +0000

    added dynamic join model

--- dbt/macros/custom_schema_name.sql
+++
@@ -1,8 +0,0 @@
-{% macro generate_schema_name(schema_name, node) -%}
-    {%- set default_schema = target.schema -%}
-    {%- if target.name in ('prod') and schema_name is not none -%}
-        {{ schema_name | trim }}
-    {%- else -%}
-        {{ default_schema }}_{{ custom_schema_name | trim }}
-    {%- endif -%}
-{%- endmacro %}
