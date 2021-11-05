8a898d8522a8c5a9ef85e802eb9c95441340cba6
Mike Fuller <mike.fuller@fishtownanalytics.com>
2021-11-05 02:01:50 +0000

    added dynamic join

---
+++ dbt/models/dynamic_joins/dynamic_join.sql
@@ -0,0 +1,16 @@
+select id
+from
+{{ ref('my_first_dbt_model') }} my_first_model
+
+{% if var('var_int') >= 1 and var('var_int') <= 3 -%}
+
+join {{ ref('my_second_dbt_model') }} my_second_model
+on my_first_model.id = my_second_model.id
+
+{% else -%}
+
+join {{ ref('my_third_dbt_model') }} my_third_dbt_model
+on my_first_model.id = my_third_dbt_model.id
+
+{% endif %}
+
