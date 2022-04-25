23b0b17b06b9c49cb8db63b5ccb211575aaf5744
Mike Fuller <mike.fuller@fishtownanalytics.com>
2022-04-25 19:33:56 +0000

    add sample model and use sk

--- dbt/macros/monotonic_surrogate_keys/sk_generator.sql
+++ dbt/macros/monotonic_surrogate_keys/sk_generator.sql
@@ -2,12 +2,24 @@
 
 {% set create_sequence_sql %}
 
-create sequence if not exists {{target.database}}.{{target.schema}}.{{this.table}}_sk 
+create sequence if not exists {{target.database}}.{{target.schema}}.{{this.table}}_seq
 start with = {{ val_start }}
 increment = {{ val_increment }}
 
 {% endset %}
 
 {{ return(create_sequence_sql) }}
+
+{% endmacro %}
+
+{% macro monotonic_sk() %}
+
+{% set sk_field_plus_alias %}
+
+{{target.database}}.{{target.schema}}.{{this.table}}_seq.nextval as {{this.table}}_sk
+
+{% endset %}
+
+{{ return(sk_field_plus_alias) }}
 
 {% endmacro %}
--- dbt/models/surrogate_key_example/monotonic_example.sql
+++ dbt/models/surrogate_key_example/monotonic_example.sql
@@ -1,6 +1,14 @@
 
 {{ config(
-    pre_hook=sk_generator(1,1)
+    pre_hook=sk_generator(1,1),
+    materialized = 'table'
 ) }}
 
-select 1 as dummy
+select {{ monotonic_sk() }},
+'first_record' as descrip
+union all
+select {{ monotonic_sk() }},
+'second_record' as descrip
+union all
+select {{ monotonic_sk() }},
+'third_record' as descrip
