758893c61b5bd381d8fec87b00e94c7f2fb6e67e
Mike Fuller <mike.fuller@fishtownanalytics.com>
2022-08-30 17:37:42 +0000

    change compile macro

--- dbt/macros/test_macro.sql
+++ dbt/macros/test_macro.sql
@@ -4,7 +4,7 @@
   select current_timestamp
 {% endset %}
 
-{% if execute and flags.WHICH not in ['compile']  %}
+{% if execute and flags.WHICH in ['compile']  %}
     {% set return_value = 'yep' %}}}
 {% else %}
     {% set return_value = 'nope' %}
