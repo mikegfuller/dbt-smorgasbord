588fdcae951ab4860d5ca7050e8979f7fea89e5f
Mike Fuller <25351862+mikegfuller@users.noreply.github.com>
2022-10-26 19:13:33 -0500

    updated comments

--- python/get_users.py
+++ python/get_users.py
@@ -2,17 +2,17 @@ import requests
 import json
 import os
 
-# set token and variables (need to get rid of this hardcoding nonsense)
+# set token and account
 api_token = os.getenv('DBT_CLOUD_API_KEY')
 account_id = <your_account>
 
 # base url for dbt cloud
 base_url = "https://cloud.getdbt.com/api/v2/"
 
-#endpoint to get manifest json file
-users_endpoint = f"accounts/{account_id}/users"
+#endpoint to get users
+users_endpoint = f"accounts/{account_id}/users?limit=1000"
 
-#manifest url
+#users url
 users_url = base_url + users_endpoint
 
 # set headers
@@ -21,17 +21,20 @@ headers = {
     'Content-Type': 'application/json'
 }
 
-# hit artifacts endpoint and return manifest file
+# hit users endpoint
 get_users = requests.get(users_url, headers=headers)
 payload_user_response = json.loads(get_users.content)
-#formatted_users = json.dumps(payload_user_response, indent=2)
 
+
+#
 for user in payload_user_response['data']:
-    print(
+        print(
     "UserID: " + str(user['id']) + "\n"
     "FirstName: " + user['first_name'] + "\n"
     "LastName: " + user['last_name'] + "\n"
     "LastLogin: " + user['last_login'] + "\n"
+    "License Type: " + user['permissions'][0]['license_type'] + "\n"
+    "Active: " + str(user['is_active']) + "\n"
     "----------------------------------------")
 
 
